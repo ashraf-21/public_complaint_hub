28316d72da9cc6f91dab5391b8a9e1bedb387e42
ashraf-21 <ashrafvali2106shaik@gmail.com>
2026-04-12 18:56:25 +0530

    User home page updated

--- home.php
+++ home.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['user']) || $_SESSION['role'] !== 'user') {
 <body>
 
     <nav class="navbar">
-        <div class="logo">📢 User Portal</div>
+        <div class="logo">User Portal</div>
         <ul class="nav-links">
             <li><a href="home.php">Home</a></li>
             <li><a href="raise_complaint.php">Raise Complaint</a></li>
